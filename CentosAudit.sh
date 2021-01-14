998f1b278a38852caf1a362577c568c06f67576b
estelle <estellebikibili18@gmail.com>
2021-01-14 11:46:54 -0500

    new script

---
+++ CentosAudit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#Description: Project
+#Author: Estelle (sitting at dinner table with two screens)
+#Date: Jan 14 2021 11:44am
+
+
+if [ $UID -ne 0 ] ; then
+	echo "You need root access"
+fi
